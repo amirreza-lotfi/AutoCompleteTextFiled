0de4464377635fd7ef9cc01fd4576a6817775ef4
amirreza <amirreza.lotfi79@gmail.com>
2022-04-28 18:13:57 +0430

    release version

---
+++ AutoCompleteTextFiled/src/main/java/com/example/autocompletetextfiled/AutoCompleteTextFiled.kt
@@ -0,0 +1,70 @@
+package com.example.autocompletetextfield
+
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.ScrollableDefaults
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AutoCompleteTextFiled(
+    textFiledValue:String,
+    itemComposable: @Composable (item:String) -> Unit,
+    autoCompleteAdapter: AutoCompleteAdapter,
+    columnContainerModifier: Modifier = Modifier,
+    contentPaddingOfContainerColumn: PaddingValues = PaddingValues(0.dp),
+    reverseLayoutOfContainerColumn: Boolean = false,
+    verticalArrangementOfContainerColumn: Arrangement.Vertical = if (!reverseLayoutOfContainerColumn) Arrangement.Top else Arrangement.Bottom,
+    horizontalAlignmentOfContainerColumn: Alignment.Horizontal = Alignment.Start,
+    flingBehaviorOfContainerColumn: FlingBehavior = ScrollableDefaults.flingBehavior(),
+) {
+    autoCompleteAdapter.onTextFiledChanged(textFiledValue)
+    LazyColumn(
+        modifier = columnContainerModifier,
+        contentPadding = contentPaddingOfContainerColumn,
+        flingBehavior = flingBehaviorOfContainerColumn,
+        horizontalAlignment = horizontalAlignmentOfContainerColumn,
+        verticalArrangement = verticalArrangementOfContainerColumn,
+        reverseLayout = reverseLayoutOfContainerColumn
+    ){
+        items(autoCompleteAdapter.getList()){ item ->
+            itemComposable(item)
+        }
+    }
+}
+
+@Composable
+fun AutoCompleteTextFiled(
+    textFiledValue:String,
+    itemComposable: @Composable (item:String) -> Unit,
+    itemList: List<String>,
+    columnContainerModifier: Modifier = Modifier,
+    contentPaddingOfContainerColumn: PaddingValues = PaddingValues(0.dp),
+    reverseLayoutOfContainerColumn: Boolean = false,
+    verticalArrangementOfContainerColumn: Arrangement.Vertical = if (!reverseLayoutOfContainerColumn) Arrangement.Top else Arrangement.Bottom,
+    horizontalAlignmentOfContainerColumn: Alignment.Horizontal = Alignment.Start,
+    flingBehaviorOfContainerColumn: FlingBehavior = ScrollableDefaults.flingBehavior(),
+) {
+    val autoCompleteAdapter = remember {
+        AutoCompleteAdapter(itemList)
+    }
+
+    autoCompleteAdapter.onTextFiledChanged(textFiledValue)
+    LazyColumn(
+        modifier = columnContainerModifier,
+        contentPadding = contentPaddingOfContainerColumn,
+        flingBehavior = flingBehaviorOfContainerColumn,
+        horizontalAlignment = horizontalAlignmentOfContainerColumn,
+        verticalArrangement = verticalArrangementOfContainerColumn,
+        reverseLayout = reverseLayoutOfContainerColumn
+    ){
+        items(autoCompleteAdapter.getList()){ item ->
+            itemComposable(item)
+        }
+    }
+}
